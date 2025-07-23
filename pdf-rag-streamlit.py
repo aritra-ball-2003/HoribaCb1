d969f66f0d1a48b5c4aa48df0bc2dbf187f9c040
Aritra Ball <64001999+aritra-ball-2003@users.noreply.github.com>
2025-07-23 18:04:29 +0530

    Update pdf-rag-streamlit.py

--- pdf-rag-streamlit.py
+++ pdf-rag-streamlit.py
@@ -6,14 +6,14 @@ import logging
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_ollama import OllamaEmbeddings
+# from langchain_ollama import OllamaEmbeddings
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain.retrievers.multi_query import MultiQueryRetriever
 import ollama
-from langchain.embeddings import OpenAIEmbeddings
+# from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 
 # Configure logging
